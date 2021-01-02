33ab5449127cbe8b67da94f7ca21a379ce6dff59
ChenSingYo <chensingyo@gmail.com>
2021-01-03 00:08:17 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const app = express()
+const port = 3000
+
+mongoose.connect('mongodb://localhost/expense', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  // useFindAndModify: false
+})
+
+// setting view engine use it in main.hbs
+app.engine('hbs', exphbs({ defaultLaybout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// use express-req-parser
+app.use(express.urlencoded({ extended: true }))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})

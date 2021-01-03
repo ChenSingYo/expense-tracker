b304915d33e006612107db3023a4cbebe78e5b86
ChenSingYo <chensingyo@gmail.com>
2021-01-03 15:30:24 +0800

    refactor: add config/mongoose.js, and exports db

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const app = express()
 const port = 3000
-
-mongoose.connect('mongodb://localhost/expense', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-  // useFindAndModify: false
-})
-const db = mongoose.connection
-db.on('error', () => { console.log('mongodb error!') })
-db.once('open', () => { console.log('mongodb connected!') })
+require('./config/mongoose')
 
 // setting view engine use it in main.hbs
 app.engine('hbs', exphbs({ defaultLaybout: 'main', extname: '.hbs' }))
---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const db = mongoose.connection
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense'
+// 將 request 導入路由器
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+db.on('error', () => { console.log('mongodb error!') })
+db.once('open', () => { console.log('mongodb connected!') })
+
+module.exports = db

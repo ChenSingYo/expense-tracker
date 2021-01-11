0ca8542ff1aa4a45587ed16480fe3d488f50d1a2
ChenSingYo <chensingyo@gmail.com>
2021-01-11 11:46:53 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,18 +8,26 @@ const Record = require('../../models/record')
 
 // main page route
 router.get('/', (req, res) => {
-  Category.find()
+  const sortBy = req.query.sort // 取得表單提交的類別項目
+  Category.find() // 搜尋category資料庫
     .lean()
-    .sort({ _id: 'asc' })
-    .then(Categories => {
-      Record.find()
-        .lean()
-        .sort({ _id: 'asc' })
-        .then(records => {
-          let totalAmount = 0
-          records.forEach(record => { totalAmount += record.amount })
-          res.render('index', { records, totalAmount, Categories })
-        })
+    .sort({ _id: 'asc' }) // 按照id排序
+    .then(Categories => { // 預備搜尋到的類別項目
+      let records = null
+      if (sortBy === 'all' || sortBy === undefined) { // 若類別項目為''
+        records = Record.find() // 用特定類別做為條件，去找尋record資料庫中的紀錄
+          .lean()
+          .sort({ _id: 'desc' }) // 按照id排序
+      } else {
+        records = Record.find({ category: `${sortBy}` }) // 用特定類別做為條件，去找尋record資料庫中的紀錄
+          .lean()
+          .sort({ _id: 'desc' }) // 按照id排序
+      }
+      records.then(records => { // 將每筆record
+        let totalAmount = 0 // 設定總金額變數為零
+        records.forEach(record => { totalAmount += record.amount }) // 將每筆record中的金額放進總金額變數
+        res.render('index', { records, totalAmount, Categories, sortBy }) // 在index頁面中放進四個變數做渲染
+      })
         .catch(error => console.error(error))
     })
     .catch(error => console.error(error))
